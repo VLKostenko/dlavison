ffbc3abba4c802b6d4beba060468ea7ff5e77921
VLKostenko <vladimirkostenko4@gmail.com>
2019-06-07 21:34:21 +0300

    Updates

---
+++ precache-manifest.2763acdf4c0ad2828dc6d56c191be07d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1221469e155cb9183b4200f6114e2cad",
+    "url": "/dlavison/index.html"
+  },
+  {
+    "revision": "485b8d0521dfa1e6957d",
+    "url": "/dlavison/static/css/main.02e99e30.chunk.css"
+  },
+  {
+    "revision": "04b3551b012eef0df1e5",
+    "url": "/dlavison/static/js/2.97ec8933.chunk.js"
+  },
+  {
+    "revision": "485b8d0521dfa1e6957d",
+    "url": "/dlavison/static/js/main.352baeb1.chunk.js"
+  },
+  {
+    "revision": "6fb040bff10d01ab4b79",
+    "url": "/dlavison/static/js/runtime~main.43b68f07.js"
+  },
+  {
+    "revision": "70adb4c4dd51b77717da185832716dbe",
+    "url": "/dlavison/static/media/AEON-bg.70adb4c4.svg"
+  },
+  {
+    "revision": "0830e78546d048c0c95498894d3d6872",
+    "url": "/dlavison/static/media/Aeon-logo.0830e785.svg"
+  },
+  {
+    "revision": "cb295d9424e8e1cf744150be637ad41c",
+    "url": "/dlavison/static/media/Aeon_Ver-2.cb295d94.png"
+  },
+  {
+    "revision": "da4c156c21ddc76509d6fa6722ebadf3",
+    "url": "/dlavison/static/media/Facebook.da4c156c.svg"
+  },
+  {
+    "revision": "92685d936079dae3434bd2f49dfe0dee",
+    "url": "/dlavison/static/media/about-foto.92685d93.svg"
+  },
+  {
+    "revision": "dc4db82faaff9363f9ab8143a800ac52",
+    "url": "/dlavison/static/media/adult-attractive-cap-2146720.dc4db82f.png"
+  },
+  {
+    "revision": "63c19da8028860e83263a50f7e6e4c5e",
+    "url": "/dlavison/static/media/google-play.63c19da8.svg"
+  },
+  {
+    "revision": "acb6e28092cada8700150ce33a3d09ed",
+    "url": "/dlavison/static/media/headphones.acb6e280.svg"
+  },
+  {
+    "revision": "7534d1969326178fda49a3b7308ee994",
+    "url": "/dlavison/static/media/heart.7534d196.svg"
+  },
+  {
+    "revision": "a61694d6e1e89347bcc88e3391ddb565",
+    "url": "/dlavison/static/media/instagram.a61694d6.svg"
+  },
+  {
+    "revision": "594b0087c4f90ac9fbefed5a68d4c6bd",
+    "url": "/dlavison/static/media/instagram2.594b0087.svg"
+  },
+  {
+    "revision": "e1887d49723d3cfcf369cd05ec9d993d",
+    "url": "/dlavison/static/media/melody.e1887d49.svg"
+  },
+  {
+    "revision": "721915bf764181b12119e05f57f0cf49",
+    "url": "/dlavison/static/media/music-player.721915bf.svg"
+  },
+  {
+    "revision": "1b3e62d8a4a27c364c56e1c84deaf9eb",
+    "url": "/dlavison/static/media/play-btn.1b3e62d8.svg"
+  },
+  {
+    "revision": "55ce062b1d12c1f4bdc84d895754dba6",
+    "url": "/dlavison/static/media/sing.55ce062b.svg"
+  },
+  {
+    "revision": "305bd6a8ee9d620b24329efcb305a243",
+    "url": "/dlavison/static/media/telegram.305bd6a8.svg"
+  },
+  {
+    "revision": "694b1d6894da821842c410a1b26fba8f",
+    "url": "/dlavison/static/media/twitter.694b1d68.svg"
+  }
+]);
